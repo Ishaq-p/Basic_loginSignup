78bc6a79c8868b00818b9bee2e0ee2df4be7e9e7
isHaq paktin <65335868+Ishaq-p@users.noreply.github.com>
2022-04-09 02:45:13 +0300

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,40 @@
+import math
+import getpass
+from math import pi
+import re
+from quadratic import quadratic
+from operationIn_OneLine import operationIn_OneLine
+from account import account
+from operations_4 import operations_4
+from mode import mode
+from history import clear_accnt_history
+from history import clear_history
+from history import history
+import geometric
+import physics
+
+
+def input_numbers(*args):
+	pass
+
+def meanwhile_4op():
+	pass
+
+def trigonometric(sign, theta):
+	pass
+
+
+def roots(a):
+	pass
+
+def exponention(*args):
+	pass
+
+
+def logrthmic(*args):
+	pass
+
+
+
+
+
